b8ea97a9ec04e9ab5c1c0b31c1dc0233d391de6b
QuangPhuc <QuangPhuc>
2017-10-17 04:57:14 +0700

    ua luc 4h 57

--- MyStudents/studentdialog.h
+++ MyStudents/studentdialog.h
@@ -6,6 +6,7 @@
 namespace Ui {
 class StudentDialog;
 }
+class Student;
 
 class StudentDialog : public QDialog
 {
@@ -14,9 +15,16 @@ class StudentDialog : public QDialog
 public:
     explicit StudentDialog(QWidget *parent = 0);
     ~StudentDialog();
+    void Show(Student* student);
+
+private slots:
+    void on_pushButton_released();
+
+    void on_pushButton_2_released();
 
 private:
     Ui::StudentDialog *ui;
+    Student* m_Student;
 };
 
 #endif // STUDENTDIALOG_H
